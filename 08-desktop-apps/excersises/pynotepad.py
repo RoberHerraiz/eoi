1079b98728d702efedb963ab00ac74614400eeea
Rober Herraiz <herraiz92@gmail.com>
2020-05-25 19:35:53 +0100

    added a new file function

--- 08-desktop-apps/excersises/pynotepad.py
+++ 08-desktop-apps/excersises/pynotepad.py
@@ -7,14 +7,18 @@ app.setApplicationName('PyNotePad')
 
 editor = QPlainTextEdit()
 
+def ask_for_confirmation():
+    answer = QMessageBox.question(window,'Confirm closing',
+            'You have unsaved changes. Are you sure you want to exit?',
+            QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel)
+    return answer
+
 class MyMainWindow(QMainWindow):
 
     def closeEvent(self, e):
         if not editor.document().isModified():
             return
-        answer = QMessageBox.question(window,'Confirm closing',
-                'You have unsaved changes. Are you sure you want to exit?',
-                QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel)
+        answer = ask_for_confirmation()
         if answer == QMessageBox.Save:
             if not save():
                 e.ignore()
@@ -29,6 +33,26 @@ window.setCentralWidget(editor)
 file_menu = window.menuBar().addMenu('&File') # Con "&" le indicamos el atajo de alt + .
 file_path = None
 
+def new_document():
+    global file_path
+    if editor.document().isModified():
+        answer = ask_for_confirmation()
+        if answer == QMessageBox.Save:
+            if not save():
+                return
+        elif answer == QMessageBox.Cancel:
+            return
+    print('voy a limpiar!')
+    editor.clear()
+    
+    file_path = None
+    
+new_file_action = QAction('New file')
+new_file_action.triggered.connect(new_document)
+new_file_action.setShortcut(QKeySequence.New)
+file_menu.addAction(new_file_action)
+
+
 def show_open_dialog():
     global file_path
     filename, _ = QFileDialog.getOpenFileName(window, 'Open...')
@@ -37,7 +61,7 @@ def show_open_dialog():
         editor.setPlainText(file_contents)
         file_path = filename
 
-open_action = QAction('&Open file')
+open_action = QAction('Open file')
 open_action.triggered.connect(show_open_dialog)
 open_action.setShortcut(QKeySequence.Open)
 file_menu.addAction(open_action)
@@ -45,7 +69,7 @@ file_menu.addAction(open_action)
 
 def save():
     if file_path is None:
-        show_save_dialog()
+        return show_save_dialog()
     else:
         with open(file_path, 'w') as f:
             f.write(editor.toPlainText())
@@ -62,7 +86,7 @@ def show_save_dialog():
         return True
     return False
 
-save_action = QAction('&Save file')
+save_action = QAction('Save file')
 save_action.triggered.connect(save)
 save_action.setShortcut(QKeySequence.Save)
 file_menu.addAction(save_action)
@@ -71,6 +95,9 @@ file_menu.addAction(save_action)
 close_action = QAction("&Close")
 close_action.triggered.connect(window.close) #signal
 file_menu.addAction(close_action) # asignamos a slot
+
+
+
 
 
 def show_about_dialog():
