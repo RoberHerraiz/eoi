ac9f44ee539d10537f0d30ffb27bc759c4dd6970
Rober Herraiz <herraiz92@gmail.com>
2020-06-12 20:06:23 +0100

    original code of treeview

---
+++ 08-desktop-apps/treeview-original.py
@@ -0,0 +1,14 @@
+from os.path import expanduser
+from PyQt5.QtWidgets import *
+
+home = expanduser('..')
+
+app = QApplication([])
+model = QDirModel()
+view  = QListView()
+view.setModel(model)
+view.setRootIndex(model.index(home))
+view.show()
+app.exec()
+
+
