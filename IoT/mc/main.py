e7e50c743aaf6731b8da7ffefe9f0121406f773a
Rober Herraiz <herraiz92@gmail.com>
2020-06-15 20:55:00 +0100

    more exercises

--- IoT/mc/main.py
+++ IoT/mc/main.py
@@ -1,33 +1,56 @@
-from machine import Pin, PWM, ADC, Timer
-import utime
+import network
+import usocket as socket
+from machine import Pin
 
-boton_pulsado = False
-tiempo_inicio_boton = 0
+def web_page(estado):
+    html = """<html><head> <title>Ejemplo 1</title> <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="data:;base64,iVBORw0KGgo="><style>html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
+    h1{color: #0F3376; padding: 2vh;}p{font-size: 1.5rem;}.button{display: inline-block; background-color: #e7bd3b; border: none;
+    border-radius: 4px; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
+    .button2{background-color: #4286f4;}</style></head><body> <h1>CONTROL DE LED</h1>
+    <p>Estado LED: <strong> """ + estado + """ </strong></p><p><a href="/?led=on"><button class="button">ON</button></a></p>
+    <p><a href="/?led=off"><button class="button button2">OFF</button></a></p></body></html>"""
+    return html
 
-def boton_callback(x):
-    global boton_pulsado, tiempo_inicio_boton
-    boton_pulsado = True
-    tiempo_inicio_boton = utime.ticks_ms()
+led = Pin(2, Pin.OUT)
+led.value(1)
 
+red = network.WLAN(network.AP_IF)
+red.active(True)
+red.config(essid="MicroPython", password="12345678")
 
-boton = Pin(0, Pin.IN)
-boton.irq(boton_callback, trigger=Pin.IRQ_FALLING)
+print(red.ifconfig())
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('0.0.0.0', 80))
+sock.listen(5)
+# sock.settimeout(30)
 
 while True:
-    print("x")
-    if boton_pulsado:
-        print("boton pulsado marca {}".format(tiempo_inicio_boton))
-        ahora =  utime.ticks_ms()
-        tiempo_boton = utime.ticks_diff(ahora, tiempo_inicio_boton)
+    print('Esperando conexion...')
+    conn, addr = sock.accept()
+    print(addr)
+    request = conn.recv(1024)
+    request_str = request.decode()
+    print(request_str)
+    respuesta = '<h1>Hola, mundo</h1> yo soy Rober'
+    ledon = request_str.find("/?led=on")
+    print(ledon)
+    ledoff = request_str.find("/?led=off")
+    print(ledoff)
 
-        print(tiempo_boton)
-        if tiempo_boton < 50:
-            utime.sleep_ms(50-tiempo_boton)
-        estado = boton.value()
-        if estado:
-            print("esto es ruido, no hacer nada")
-        else:
-            print("boton pulsado de verdad")
-        utime.sleep(0.5) #antirrebotes
-        boton_pulsado = False
-    utime.sleep(0.1)
+    estado_led = led.value()
+    estado_str = "ENCENDIDO" if not estado_led else "APAGADO"
+
+    if  ledon == 4:
+        led.value(0)
+        respuesta = "Led encendido"
+    elif ledoff == 4:
+        respuesta = "Led apagado"
+        led.value(1)
+
+    conn.sendall('HTTP/1.1 200 OK\n')
+    conn.sendall('Content-Type: text/html\n')
+    conn.sendall('Connection: close\n\n')
+    conn.sendall(web_page(estado_str))
+    conn.close()
